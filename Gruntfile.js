42da7df0b8a5e570efbf32819f60c1471f5ee3fc
Ryan Raub <ryan.raub@asu.edu>
2014-12-03 11:51:19 -0700

    adding some grunt and travis config, still not quite working need to get the coding standards included from the git repo

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= props.license %> */\n',
+
+        // PHP Code Sniffer
+        // ================
+        phpcs: {
+            core: {
+                dir: ['*.php']
+            },
+            options: {
+                bin: 'vendor/bin/phpcs',
+                standard: 'Wordpress'
+            }
+        },
+        // JS Hint
+        // =======
+        jshint: {
+            options: {
+                jshintrc: 'js/.jshintrc'
+            },
+            core: {
+                src: 'js/*.js'
+            }
+        },
+        // JS Coding Style
+        // ===============
+        jscs: {
+            options: {
+                config: 'js/.jscsrc'
+            },
+            core: {
+                src: '<%= jshint.core.src %>'
+            }
+        },
+        // QUnit
+        // =====
+        // qunit: {
+        //     options: {
+        //         inject: 'test/qunit/phantom.js'
+        //     },
+        //     files: ['test/qunit/index.html']
+        // },
+        // SCSS Lint
+        // =========
+        scsslint: {
+            allFiles: [
+                'scss/*.scss',
+                'scss/mixins/*.scss',
+                'scss/theme/*.scss'
+                // Not font_awesome
+            ],
+            options: {
+              config: 'scss/.scss-lint.yml'
+          }
+        },
+        // SASS Compile
+        // ============
+        sass: {
+            options: {
+                style: 'expanded',
+                sourcemap: 'auto'
+            },
+            dist: {
+                files: {
+                    'build/css/sos-wordpres-theme.css' : 'scss/sos-wordpres-theme.scss',
+                }
+            }
+        },
+        // JS Compile
+        // ==========
+        concat: {
+            bootstrapAsu: {
+                src: [
+                    'js/_modernizr.js',
+                    'js/_smoothscroll.js',
+                    'js/_debounce.js',
+                    'js/_calendar.js',
+                    'js/_sidebar.js',
+                    'js/_collapse-footer.js'
+                ],
+                 dest: 'build/js/bootstrap-asu.js'
+            }
+        },
+        // JS Uglify
+        // =========
+        uglify: {
+          options: {
+            preserveComments: 'some'
+          },
+          core: {
+            src: 'build/js/sos-wordpres-theme.js',
+            dest: 'build/js/sos-wordpres-theme.min.js'
+          }
+        },
+        // Watch
+        // =====
+        watch: {
+            core: {
+                files: '<%= jshint.core.src %>',
+                tasks: ['jshint:core', 'qunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    // grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-scss-lint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-phpcs');
+
+    // Default task
+    grunt.registerTask('default', [
+        'jshint',
+        'jscs',
+        'scsslint',
+        // 'qunit',
+        'sass',
+        'concat',
+        'uglify',
+        'phpcs']);
+};
+
