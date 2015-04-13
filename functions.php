dc1bea4de35a11a206ccd7e8adf7c3afc6cc4f90
Ivan Montiel <idmontie@asu.edu>
2015-04-13 15:58:17 -0700

    upversion sos theme.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 
 function sos_wordpress_theme_scripts() {
-  wp_register_style( 'sos-wordpress-theme', get_stylesheet_directory_uri().'/build/css/sos-wordpress-theme.min.css', array(), '0.0.3', 'all' );
+  wp_register_style( 'sos-wordpress-theme', get_stylesheet_directory_uri().'/build/css/sos-wordpress-theme.min.css', array(), '0.0.4', 'all' );
   wp_enqueue_style( 'sos-wordpress-theme' );
   
   wp_register_script( 'sos-wordpress-theme', get_stylesheet_directory_uri().'/build/js/sos-wordpress-theme.min.js', array(), '0.0.4', 'all' );
